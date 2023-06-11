844ca99c2f09bcc15884592b44e9c91eb4f4a591
Diegantos <diegantos8@gmail.com>
2023-06-11 20:59:05 +0200

    slide2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Slide1 } from './Components/Slide1/Slide1'
 import './App.scss'
 import { useEffect, useState } from 'react'
 import { Spinner } from './Components/Spinner/Spinner'
+import { Slide2 } from './Components/Slide2/Slide2'
 
 function App() {
 
@@ -18,7 +19,10 @@ function App() {
       {isLoading ? (
         <Spinner />
       ) : (
-        <Slide1 />
+        <>
+          <Slide1 />
+          <Slide2 />
+        </>
       )
     }
     </div>
---
+++ src/Components/Slide2/Slide2.jsx
@@ -0,0 +1,9 @@
+import './Slide2.scss'
+
+export const Slide2 = () => {
+    return(
+        <div className="Slide2">
+            Hello from Slide 2
+        </div>
+    )
+}
