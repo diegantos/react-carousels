f0ec4c3f04bc6a54d2aab370fda91f74564048b2
Diegantos <diegantos8@gmail.com>
2023-06-11 21:18:29 +0200

    gsap

--- src/Components/Slide2/Slide2.jsx
+++ src/Components/Slide2/Slide2.jsx
@@ -1,9 +1,36 @@
 import './Slide2.scss'
 
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import { useEffect, useRef } from 'react'
+
 export const Slide2 = () => {
-    return(
-        <div className="Slide2">
-            Hello from Slide 2
-        </div>
-    )
+    const sectionRef = useRef(null)
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger)
+
+        const sections = sectionRef.current
+
+        gsap.to(sections, {
+            xPercent: -100, // 100 implica desplazarse a la izda
+            ease: "none",
+            scrollTrigger: {
+                trigger: sections,
+                scrub: true,
+                pin: true,
+                start: "top top",
+                end: "bottom top",
+                markers: true,
+            },
+        })
+    },[])
+
+    return (
+      <div className="Slide2">
+        Hello from Slide 2
+        <div className="Section" ref={sectionRef}>First section</div>
+        <div className="Section" ref={sectionRef}>Second section</div>
+      </div>
+    );
 }
